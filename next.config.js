8a4c0dca32101cd9d900eb628d805b6ed0d53c68
J <jzh1669@gmail.com>
2025-04-20 22:23:56 -0400

    fix image uploading config

--- next.config.js
+++ next.config.js
@@ -1,22 +1,27 @@
 /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: 'zhangj.ing',
+//         pathname: '/wp-content/uploads/**',
+//       },
+//     ],
+//   },
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/wp-content/uploads/:path*',
+//         destination: 'https://zhangj.ing/wp-content/uploads/:path*',
+//       },
+//     ];
+//   },
+// }
 const nextConfig = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'zhangj.ing',
-        pathname: '/wp-content/uploads/**',
-      },
-    ],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/wp-content/uploads/:path*',
-        destination: 'https://zhangj.ing/wp-content/uploads/:path*',
-      },
-    ];
-  },
+    domains: ['zhangj.ing'],  // For any remaining remote images
+    unoptimized: true,        // This can help with deployment issues
+  }
 }
-
 module.exports = nextConfig
